c985a81221cb6f60ebb0aa9da613eafdb42d4a78
Matteo Stelbrink <147656301+mstelbrink@users.noreply.github.com>
2024-01-11 19:45:48 +0100

    Added file

---
+++ Test.vue
@@ -0,0 +1,25 @@
+<script>
+  export default {
+    data() {
+      return {
+        meals: ""
+      };
+    },
+    methods: {
+      fetchUrl() {
+        fetch('https://openmensa.org/api/v2/canteens/63/days/02-02-2023/meals')
+            .then((response) => {
+              response.json().then((data) => {
+                this.meals = data;
+              })
+            });
+      }
+    }
+  }
+</script>
+
+<template>
+  <button type="button" @click="fetchUrl">Show meals</button>
+  <p>{{ meals }}</p>
+</template>
+
